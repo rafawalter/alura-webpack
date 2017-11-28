ce51d06265eb53afc2d08b12d7fe225cf9f2c913
Rafael Fraga Walter <rafael.walter@bndes.gov.br>
2017-11-28 12:02:35 -0200

    Boas práticas

--- projeto-webpack/client/app-src/domain/negociacao/NegociacaoService.js
+++ projeto-webpack/client/app-src/domain/negociacao/NegociacaoService.js
@@ -12,7 +12,7 @@ export class NegociacaoService {
     obtemNegociacoesDaSemana() {
 
         return this._http
-            .get('http://localhost:3000/negociacoes/semana')
+            .get(`${SERVICE_URL}/negociacoes/semana`)
             .then(
                 dados =>
                 dados.map(objeto =>
@@ -27,7 +27,7 @@ export class NegociacaoService {
     obtemNegociacoesDaSemanaAnterior() {
 
         return this._http
-            .get('http://localhost:3000/negociacoes/anterior')
+            .get(`${SERVICE_URL}/negociacoes/anterior`)
             .then(
                 dados => dados.map(objeto =>
                     new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)),
@@ -41,7 +41,7 @@ export class NegociacaoService {
     obtemNegociacoesDaSemanaRetrasada() {
 
         return this._http
-            .get('http://localhost:3000/negociacoes/retrasada')
+            .get(`${SERVICE_URL}/negociacoes/retrasada`)
             .then(
                 dados => dados.map(objeto =>
                     new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)),
