2cc265c35f2702e83adca03d3d000f86ab1f261a
Rafael Fraga Walter <rafael.walter@bndes.gov.br>
2017-11-28 10:37:30 -0200

    05. importando scripts

--- projeto-webpack/client/app-src/app.js
+++ projeto-webpack/client/app-src/app.js
@@ -1,10 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
+import 'bootstrap/js/modal.js';
 import '../css/meucss.css';
 
 import { NegociacaoController } from './controllers/NegociacaoController.js';
 import { Negociacao } from './domain/index.js';
+
+$('h1').on('click', () => alert('Foi clicado!'));
+
+console.log($('h1').modal);
 
 const controller = new NegociacaoController();
 const negociacao = new Negociacao(new Date(), 1, 200);
